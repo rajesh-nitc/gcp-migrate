0ee63389c44105e2cf343dbefed0f8b0ed9549ac
Rajesh Gupta <rajesh.nitc@gmail.com>
2020-12-26 17:35:34 +0530

    keeping gcp fw rules from module

--- 2-bootstrap-aws/gcp_security.tf
+++ 2-bootstrap-aws/gcp_security.tf
@@ -1,10 +1,10 @@
-resource "google_compute_firewall" "test" {
-  name    = "${google_compute_network.gcp-network.name}-test"
+# Allow PING testing.
+resource "google_compute_firewall" "gcp-allow-icmp" {
+  name    = "${google_compute_network.gcp-network.name}-gcp-allow-icmp"
   network = google_compute_network.gcp-network.name
 
   allow {
-    protocol = "all"
-    ports    = []
+    protocol = "icmp"
   }
 
   source_ranges = [
@@ -12,87 +12,73 @@ resource "google_compute_firewall" "test" {
   ]
 }
 
-# # Allow PING testing.
-# resource "google_compute_firewall" "gcp-allow-icmp" {
-#   name    = "${google_compute_network.gcp-network.name}-gcp-allow-icmp"
-#   network = google_compute_network.gcp-network.name
+# Allow SSH for iperf testing.
+resource "google_compute_firewall" "gcp-allow-ssh" {
+  name    = "${google_compute_network.gcp-network.name}-gcp-allow-ssh"
+  network = google_compute_network.gcp-network.name
 
-#   allow {
-#     protocol = "icmp"
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
 
-#   source_ranges = [
-#     "0.0.0.0/0",
-#   ]
-# }
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+}
 
-# # Allow SSH for iperf testing.
-# resource "google_compute_firewall" "gcp-allow-ssh" {
-#   name    = "${google_compute_network.gcp-network.name}-gcp-allow-ssh"
-#   network = google_compute_network.gcp-network.name
+# Allow traffic from the VPN subnets.
+resource "google_compute_firewall" "gcp-allow-vpn" {
+  name    = "${google_compute_network.gcp-network.name}-gcp-allow-vpn"
+  network = google_compute_network.gcp-network.name
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
 
-#   source_ranges = [
-#     "0.0.0.0/0",
-#   ]
-# }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
 
-# # Allow traffic from the VPN subnets.
-# resource "google_compute_firewall" "gcp-allow-vpn" {
-#   name    = "${google_compute_network.gcp-network.name}-gcp-allow-vpn"
-#   network = google_compute_network.gcp-network.name
+  source_ranges = [
+    var.aws_subnet1_cidr,
+  ]
+}
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["0-65535"]
-#   }
+# Allow TCP traffic from the Internet.
+resource "google_compute_firewall" "gcp-allow-internet" {
+  name    = "${google_compute_network.gcp-network.name}-gcp-allow-internet"
+  network = google_compute_network.gcp-network.name
 
-#   allow {
-#     protocol = "udp"
-#     ports    = ["0-65535"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
-#   source_ranges = [
-#     var.aws_subnet1_cidr,
-#   ]
-# }
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+}
 
-# # Allow TCP traffic from the Internet.
-# resource "google_compute_firewall" "gcp-allow-internet" {
-#   name    = "${google_compute_network.gcp-network.name}-gcp-allow-internet"
-#   network = google_compute_network.gcp-network.name
+resource "google_compute_firewall" "https" {
+  name    = "gcp-network-https"
+  network = google_compute_network.gcp-network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
-
-#   source_ranges = [
-#     "0.0.0.0/0",
-#   ]
-# }
-
-# resource "google_compute_firewall" "https" {
-#   name    = "gcp-network-https"
-#   network = google_compute_network.gcp-network.name
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["443"]
-#   }
-#   direction = "INGRESS"
-#   source_ranges = ["0.0.0.0/0"]
-# }
-
-# resource "google_compute_firewall" "gcp-allow-all" {
-#   name    = "gcp-network-allow-all"
-#   network = google_compute_network.gcp-network.name
-#   allow {
-#     protocol = "all"
-#   }
-#   direction = "INGRESS"
-#   source_ranges = ["0.0.0.0/0"]
-# }
+resource "google_compute_firewall" "gcp-allow-all" {
+  name    = "gcp-network-allow-all"
+  network = google_compute_network.gcp-network.name
+  allow {
+    protocol = "all"
+  }
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
